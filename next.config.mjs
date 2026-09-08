cdd4a20df93d57695ae353b6449a663811f0bd84
gujjuskills <mail2jaydip@gmail.com>
2026-09-09 00:31:22 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,19 @@
- const nextConfig = {
-  output: "export",
+ const isProd = process.env.NODE_ENV === "production";
+
+const basePath = isProd ? "/gujjuskills" : "";
+
+const nextConfig = {
+  basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
