3dd372f837d0fff137783641b23b3e030f0eba07
gujjuskills <mail2jaydip@gmail.com>
2026-09-18 20:38:34 +0530

    Add GoogleAd component import to index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import config from "@config/config.json";
 import Base from "@layouts/Baseof";
 import ImageFallback from "@layouts/components/ImageFallback";
 import Pagination from "@layouts/components/Pagination";
+import GoogleAd from "@layouts/components/GoogleAd";
 import Post from "@layouts/partials/Post";
 import Sidebar from "@layouts/partials/Sidebar";
 import { getListPage, getSinglePage } from "@lib/contentParser";
