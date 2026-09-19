1f197e0ef515290c9b5b3c57545af789856cfa26
gujjuskills <mail2jaydip@gmail.com>
2026-09-19 10:31:56 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -56,13 +56,7 @@ const App = ({ Component, pageProps }) => {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=5"
         />
-         {/* Google AdSense */}
-  <Script
-    async
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8383584974642753"
-    crossOrigin="anonymous"
-  />            
-      </Head>
+        </Head>
       <ThemeProvider attribute="class" defaultTheme={default_theme}>
         <Component {...pageProps} />
       </ThemeProvider>
