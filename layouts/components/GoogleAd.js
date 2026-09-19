cfa4be82f9ae4370867defd5f702fbb62d9c64df
gujjuskills <mail2jaydip@gmail.com>
2026-09-19 11:50:29 +0530

    Update GoogleAd.js

--- layouts/components/GoogleAd.js
+++ layouts/components/GoogleAd.js
@@ -1,34 +1,26 @@
-import Script from "next/script";
+import { useEffect } from "react";
 
 const GoogleAd = () => {
-  return (
-    <>
-      <ins
-        className="adsbygoogle"
-        style={{
-          display: "block",
-          width: "100%",
-        }}
-        data-ad-client="ca-pub-8383584974642753"
-        data-ad-slot="9550848037"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (error) {
+      console.error("AdSense error:", error);
+    }
+  }, []);
 
-      <Script
-        id="google-adsense"
-        strategy="afterInteractive"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8383584974642753"
-        crossOrigin="anonymous"
-        onLoad={() => {
-          try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-          } catch (error) {
-            console.error("AdSense error:", error);
-          }
-        }}
-      />
-    </>
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{
+        display: "block",
+        width: "100%",
+      }}
+      data-ad-client="ca-pub-8383584974642753"
+      data-ad-slot="9550848037"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 };
 
