56589cbec138df4b2de4c90b191fbb96a0921d93
gujjuskills <mail2jaydip@gmail.com>
2026-09-19 13:11:35 +0530

    Update GoogleAd.js

--- layouts/components/GoogleAd.js
+++ layouts/components/GoogleAd.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 
-const GoogleAd = () => {
+const GoogleAd = ({ slot = "7233385467" }) => {
   useEffect(() => {
     try {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
@@ -10,17 +10,25 @@ const GoogleAd = () => {
   }, []);
 
   return (
-    <ins
-      className="adsbygoogle"
+    <div
       style={{
-        display: "block",
         width: "100%",
+        maxWidth: "100%",
+        overflow: "hidden",
       }}
-      data-ad-client="ca-pub-8383584974642753"
-      data-ad-slot="7233385467"
-      data-ad-format="auto"
-      data-full-width-responsive="true"
-    />
+    >
+      <ins
+        className="adsbygoogle"
+        style={{
+          display: "block",
+          width: "100%",
+        }}
+        data-ad-client="ca-pub-8383584974642753"
+        data-ad-slot={slot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      />
+    </div>
   );
 };
 
