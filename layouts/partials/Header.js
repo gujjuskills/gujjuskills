13a39c3a292475c8af358ab57de6a25b721b4a25
gujjuskills <mail2jaydip@gmail.com>
2026-09-23 15:00:18 +0530

    Update Header.js

--- layouts/partials/Header.js
+++ layouts/partials/Header.js
@@ -4,7 +4,6 @@ import socical from "@config/social.json";
 import Social from "@layouts/components/Social";
 import ThemeSwitcher from "@layouts/components/ThemeSwitcher";
 import SearchModal from "@partials/SearchModal";
-import GoogleTranslate from "@layouts/components/GoogleTranslate";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
@@ -21,7 +20,7 @@ const Header = () => {
   // Router
   const router = useRouter();
 
-  // stop scrolling when nav is open
+  //stop scrolling when nav is open
   useEffect(() => {
     if (showMenu) {
       document.body.classList.add("menu-open");
@@ -33,30 +32,20 @@ const Header = () => {
   return (
     <header className="header">
       <nav className="navbar container px-1 sm:px-8">
-
-        {/* Logo */}
         <div className="order-0">
           <Logo />
         </div>
-
         <div className="flex items-center space-x-4 xl:space-x-8">
-
-          {/* Navigation */}
           <div
             className={`collapse-menu ${
               !showMenu && "translate-x-full"
             } lg:flex lg:translate-x-0`}
           >
-
-            {/* Mobile close button */}
             <button
               className="absolute right-6 top-11 lg:hidden"
               onClick={() => setShowMenu(false)}
             >
-              <svg
-                className="h-4 w-4 fill-current"
-                viewBox="0 0 20 20"
-              >
+              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
                 <title>Menu Close</title>
                 <polygon
                   points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
@@ -64,19 +53,14 @@ const Header = () => {
                 />
               </svg>
             </button>
-
-            {/* Main Menu */}
             <ul
               id="nav-menu"
               className="navbar-nav w-full md:w-auto md:space-x-1 lg:flex xl:space-x-2"
             >
               {main.map((menu, i) => (
                 <React.Fragment key={`menu-${i}`}>
-
                   {menu.hasChildren ? (
-
                     <li className="nav-item nav-dropdown group relative">
-
                       <span
                         className={`nav-link ${
                           menu.children
@@ -85,7 +69,6 @@ const Header = () => {
                         } inline-flex items-center`}
                       >
                         {menu.name}
-
                         <svg
                           className="h-4 w-4 fill-current"
                           viewBox="0 0 20 20"
@@ -93,9 +76,7 @@ const Header = () => {
                           <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
                         </svg>
                       </span>
-
                       <ul className="nav-dropdown-list hidden transition-all duration-300 group-hover:top-[46px] group-hover:block md:invisible md:absolute md:top-[60px] md:block md:opacity-0 md:group-hover:visible md:group-hover:opacity-100">
-
                         {menu.children.map((child, i) => (
                           <li
                             className="nav-dropdown-item"
@@ -103,6 +84,7 @@ const Header = () => {
                           >
                             <Link
                               href={child.url}
+                              onClick={() => setShowMenu(false)}
                               className={`nav-dropdown-link block ${
                                 router.asPath === child.url && "active"
                               }`}
@@ -111,15 +93,10 @@ const Header = () => {
                             </Link>
                           </li>
                         ))}
-
                       </ul>
-
                     </li>
-
                   ) : (
-
                     <li className="nav-item">
-
                       <Link
                         href={menu.url}
                         className={`nav-link block ${
@@ -128,27 +105,16 @@ const Header = () => {
                       >
                         {menu.name}
                       </Link>
-
                     </li>
-
                   )}
-
                 </React.Fragment>
               ))}
             </ul>
-
-            {/* Header Social */}
+            {/* header social */}
             <Social source={socical} className="socials" />
-
           </div>
-
-          {/* Theme Switcher */}
           <ThemeSwitcher />
-
-          {/* Google Translate */}
-          <GoogleTranslate />
-
-          {/* Header Search */}
+          {/* Header search */}
           <div
             className="search-icon"
             onClick={() => {
@@ -157,17 +123,12 @@ const Header = () => {
           >
             <IoSearch />
           </div>
-
-          {/* Mobile Menu Button */}
           <button
             onClick={() => setShowMenu(!showMenu)}
             className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white lg:hidden"
           >
             {showMenu ? (
-              <svg
-                className="h-4 w-4 fill-current"
-                viewBox="0 0 20 20"
-              >
+              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
                 <title>Menu Close</title>
                 <polygon
                   points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
@@ -175,31 +136,22 @@ const Header = () => {
                 />
               </svg>
             ) : (
-              <svg
-                className="h-4 w-4 fill-current"
-                viewBox="0 0 20 20"
-              >
+              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
                 <title>Menu Open</title>
                 <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
               </svg>
             )}
           </button>
-
         </div>
 
-        {/* Search Modal */}
         <SearchModal
           searchModal={searchModal}
           setSearchModal={setSearchModal}
         />
-
       </nav>
-
-      {/* Mobile Menu Background */}
       {showMenu && (
         <div className="header-backdrop absolute top-0 left-0 h-[100vh] w-full bg-black/50 lg:hidden"></div>
       )}
-
     </header>
   );
 };
